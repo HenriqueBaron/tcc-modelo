727c71452216a3e63578b15ef2868796b2498a27
Henrique Baron <henrique.baron@gmail.com>
2018-08-20 01:09:27 -0300

    Resolução numérica do sistema com ode45.

--- Desbalanceamento/Analise2DOF.m
+++ Desbalanceamento/Analise2DOF.m
@@ -2,19 +2,15 @@
 
 clearvars;
 t0 = 0; % Instante inicial
-tf = 0.05; % Instante final
-dt = 0.001; % Resolução do tempo
+tf = 0.1; % Instante final
 m = 1.565; % Massa total do rotor
 e = 10e-3; % Afastamento do centro de massa do rotor
-c_i = 0.2; % Amortecimento interno do eixo
+c_i = 0.1; % Amortecimento interno do eixo
 c = 0; % Amortecimento externo do sistema
 d_eixo = 0.02; % Diâmetro do eixo
 L_eixo = 0.4; % Comprimento do eixo
 E = 200e9; % Módulo de elasticidade do aço
 N = 1800; % Velocidade de rotação, em revoluções por minuto
-
-% Vetor tempo
-t_vals = t0:dt:tf;
 
 % Rigidez do eixo
 A = pi*d_eixo^2/4; % Seção transversal do eixo
@@ -23,33 +19,18 @@ k = A*E/L_eixo;
 % Velocidade angular
 omega = N*pi/30;
 
-% Funções de posição nos dos eixos e suas derivadas
-syms x(t) y(t);
-dx = diff(x);
-ddx = diff(dx);
-dy = diff(y);
-ddy = diff(dy);
 
-% Equações diferenciais
-fx = m*ddx + (c_i+c)*dx + k*x - c_i*omega*y == m*omega^2*e*cos(omega*t);
-fy = m*ddy + (c_i+c)*dy + k*y - c_i*omega*x == m*omega^2*e*sin(omega*t);
-f_total = [fx fy];
+eqs = @(t,y) [y(2);
+    omega^2*e*cos(omega*t) - (c_i+c)/m*y(2) - k/m*y(1) + c_i*omega/m*y(3);
+    y(4);
+    omega^2*e*sin(omega*t) - (c_i+c)/m*y(4) - k/m*y(3) + c_i*omega/m*y(1)];
 
-% Condições iniciais
-cond1 = x(0) == 0;
-cond2 = dx(0) == 0;
-cond3 = y(0) == 0;
-cond4 = dy(0) == 0;
-conds = [cond1 cond2 cond3 cond4];
+[t,y] = ode45(eqs,[t0 tf], [0; 0; 0; 0]);
 
-[sol_x(t), sol_y(t)] = dsolve(f_total,conds);
-sol_x = simplify(sol_x);
-sol_y = simplify(sol_y);
+figure(1)
+grid on;
+plot(t,y(:,1));
 
-% figure(1)
-% grid on;
-% plot(t_vals,sol_x(t_vals));
-% 
-% figure(2)
-% grid on;
-% plot(t_vals,sol_y(t_vals));
+figure(2)
+grid on;
+plot(t,y(:,3));
