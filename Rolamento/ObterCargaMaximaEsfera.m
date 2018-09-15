62ceffdce7b07c0da9ca5711bf847bd850df9c3a
Henrique Baron <henrique.baron@gmail.com>
2018-09-15 15:29:52 -0300

    Comentado o cálculo de carga máxima na esfera. Processo iterativo não funcionava com baixa carga.

--- Rolamento/ObterCargaMaximaEsfera.m
+++ Rolamento/ObterCargaMaximaEsfera.m
@@ -12,27 +12,28 @@ function [ wz_max ] = ObterCargaMaximaEsfera( wz, n, cd, Eef, F, epsilon, R, k)
 %   R - Raios equivalentes, pistas interna e externa
 %   k - Parametros de elipticidade, pistas interna e externa
 
-delta = zeros(2,1);
-Zw = 5;
-razaoComp = 0;
-erroRel = 1; % Erro relativo do processo iterativo
-while erroRel > 1e-5
-    razaoCompAnt = razaoComp;
+% delta = zeros(2,1);
+% Zw = 5;
+Zw = 4.37;
+% razaoComp = 0;
+% erroRel = 1; % Erro relativo do processo iterativo
+% while erroRel > 1e-5
+%     razaoCompAnt = razaoComp;
     wz_max = Zw*wz/n;
-    
-    % Compressoes locais elasticas maximas
-    for i=1:2
-        delta(i) = F(i)*(9/(2*epsilon(i)*R(i))*(wz_max/(pi*k(i)*Eef))^2)^(1/3);
-    end
-    deltaSum = sum(delta);
-    
-    razaoComp = cd/(2*deltaSum);
-    
-    % Reavaliacao do valor de Zw
-    Zw = pi*(1-razaoComp)^(3/2)/(2.491*(sqrt(1+((1-razaoComp)/1.23)^2)-1));
-    
-    erroRel = abs(razaoComp - razaoCompAnt)/razaoComp;
-end
+%     
+%     % Compressoes locais elasticas maximas
+%     for i=1:2
+%         delta(i) = F(i)*(9/(2*epsilon(i)*R(i))*(wz_max/(pi*k(i)*Eef))^2)^(1/3);
+%     end
+%     deltaSum = sum(delta);
+%     
+%     razaoComp = cd/(2*deltaSum);
+%     
+%     % Reavaliacao do valor de Zw
+%     Zw = pi*(1-razaoComp)^(3/2)/(2.491*(sqrt(1+((1-razaoComp)/1.23)^2)-1));
+%     
+%     erroRel = abs(razaoComp - razaoCompAnt)/razaoComp;
+% end
 
 end
 
