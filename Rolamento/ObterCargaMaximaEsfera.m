37b16a95d096d52383ee071d02f9432544ae36d2
Henrique Baron <henrique.baron@gmail.com>
2018-10-05 07:09:30 -0300

    Início da reimplementação do cálculo de carga máxima na esfera.

--- Rolamento/ObterCargaMaximaEsfera.m
+++ Rolamento/ObterCargaMaximaEsfera.m
@@ -1,4 +1,4 @@
-function [ wz_max ] = ObterCargaMaximaEsfera( wz, n, cd, Eef, F, epsilon, R, k)
+function [ wz_max ] = ObterCargaMaximaEsfera( wz, n, cd, Eef, R, IF, IE, k)
 %ObterCargaMaximaEsfera Determina a carga maxima de esfera do rolamento
 %   Realiza um processo iterativo para determinar a carga na esfera mais
 %   carregada do rolamento.
@@ -8,32 +8,20 @@ function [ wz_max ] = ObterCargaMaximaEsfera( wz, n, cd, Eef, F, epsilon, R, k)
 %   n - Numero de esferas
 %   cd - Folga diametral, mm
 %   Eef - Modulo de elasticidade efetivo, MPa
-%   F, epsilon - Resultados integrais elipticas, pistas interna e externa
-%   R - Raios equivalentes, pistas interna e externa
+%   R - Somas de curvatura, pistas interna e externa
+%   IF - Integrais elípticas de primeira ordem, pistas interna e externa
+%   IE - Integrais elípticas de segunda ordem, pistas interna e externa
 %   k - Parametros de elipticidade, pistas interna e externa
 
-% delta = zeros(2,1);
-% Zw = 5;
-Zw = 4.37;
-% razaoComp = 0;
-% erroRel = 1; % Erro relativo do processo iterativo
-% while erroRel > 1e-5
-%     razaoCompAnt = razaoComp;
-    wz_max = Zw*wz/n;
-%     
-%     % Compressoes locais elasticas maximas
-%     for i=1:2
-%         delta(i) = F(i)*(9/(2*epsilon(i)*R(i))*(wz_max/(pi*k(i)*Eef))^2)^(1/3);
-%     end
-%     deltaSum = sum(delta);
-%     
-%     razaoComp = cd/(2*deltaSum);
-%     
-%     % Reavaliacao do valor de Zw
-%     Zw = pi*(1-razaoComp)^(3/2)/(2.491*(sqrt(1+((1-razaoComp)/1.23)^2)-1));
-%     
-%     erroRel = abs(razaoComp - razaoCompAnt)/razaoComp;
-% end
+[delta K] = deal(zeros(2,1));
 
+for i=1:2
+    % Determina a deformação elástica máxima no contato
+    K(i) = pi*k*Eef*(2*IE(i)*R(i)/(9*IF(i).^3)).^(1/2);
+    delta(i) = (wz/K(i)).^(2/3);
 end
 
+delta_m = sum(delta);
+Kj = 1/((1/K(1)).^(2/3) + (1/K(2)).^(2/3)).^(3/2);
+
+end
