e3505aefd6c30029da0402ad56342eaf9860d86f
Henrique Baron <henrique.baron@gmail.com>
2018-10-09 20:48:27 -0300

    Modificada para notação com Zw no cálculo da carga.

--- Rolamento/ObterCargaMaximaEsfera.m
+++ Rolamento/ObterCargaMaximaEsfera.m
@@ -13,16 +13,17 @@ function [ wz_max ] = ObterCargaMaximaEsfera( wz, n, cd, Eef, R, IF, IE, k )
 %   IE - Integrais elípticas de segunda ordem, pistas interna e externa
 %   k - Parametros de elipticidade, pistas interna e externa
 
-[delta,K] = deal(zeros(2,1));
+delta = deal(zeros(2,1));
 
 calcularDelta = @(wz,Eef,R,IF,IE,k) IF*((9/(2*IE*R))*... 
     (wz/(pi*k*Eef))^2)^(1/3);
 fun = @(psi,C) (cos(psi)-C).^(3/2).*cos(psi);
 
 wz_max = wz;
-wz_max_ant = 0;
+Zw = 5;
+Zw_ant = 0;
 
-while abs(wz_max-wz_max_ant) > 1e-5
+while abs(Zw - Zw_ant) > 1e-7
 for i=1:2
     % Determina a deformação elástica máxima no contato
     delta(i) = calcularDelta(wz_max,Eef,R(i),IF(i),IE(i),k(i));
@@ -31,9 +32,11 @@ end
 delta_m = sum(delta);
 psi_l = acos(cd/(2*delta_m));
 
-wz_max_ant = wz_max;
+Zw_ant = Zw;
+
 intFun = integral(@(psi)fun(psi,cd/(2*delta_m)),0,psi_l);
-wz_max = wz*pi*(1-cd/(2*delta_m))^(3/2)/(n*intFun);
+Zw = pi*(1-cd/(2*delta_m))^(3/2)/intFun;
+wz_max = wz*Zw/n;
 
 end
 
