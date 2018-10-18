11ae550d15e0f2b85ec9dc6af8c53574feadc71a
Henrique Baron <henrique.baron@gmail.com>
2018-10-18 05:57:05 -0300

    Incluir função para montagem do sistema de equações diferenciais.

---
+++ Rolamento/SisLinOrdem2.m
@@ -0,0 +1,9 @@
+function dydt = SisLinOrdem2(M, C, K, F)
+%SISLINORDEM2 Monta um vetor de EDOs de ordem 2 para sistema de vibracao.
+%   Utiliza as matrizes de massa, amortecimento, rigidez e forca externa
+%   fornecidas para montar um vetor de equacoes diferenciais do tipo
+%   y'=f(t,y) que pode ser resolvido com solvers do MATLAB como o ode45.
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
