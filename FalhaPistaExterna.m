c29fc0370eb83dcac155060fe78a099369ab342d
Henrique Baron <henrique.baron@gmail.com>
2018-11-06 19:50:17 -0200

    Corrigir formatação dos textos nos plots.

--- FalhaPistaExterna.m
+++ FalhaPistaExterna.m
@@ -165,8 +165,8 @@ f = (0:L/2)*(Fs/L);
 figure(1);
 plot(t,fImpacto(t));
 title('Perfil dos impulsos de impacto','Interpreter','latex');
-xlabel('Tempo [s]','Interpreter','latex');
-ylabel('For\c{c}a [N]','Interpreter','latex');
+xlabel('Tempo $\rm [s]$','Interpreter','latex');
+ylabel('For\c{c}a $\rm [N]$','Interpreter','latex');
 xlim([0 0.1]);
 ylim('auto');
 
@@ -175,26 +175,26 @@ xlimTempo = [0 0.5];
 ylimTempo = 'auto';
 subplot(3,1,1);
 plot(t,ext.pos);
-title('Deslocamento','Interpreter','latex');
-ylabel('[m]','Interpreter','latex');
+title('Anel externo','Interpreter','latex');
+ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
 xlim(xlimTempo);
 ylim(ylimTempo);
 
 subplot(3,1,2);
 plot(t,ext.vel);
-title('Velocidade','Interpreter','latex');
-ylabel('[m/s]','Interpreter','latex');
+ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
 xlim(xlimTempo);
 ylim(ylimTempo);
 
 subplot(3,1,3);
 plot(t,ext.acc);
-title('Acelera\c{c}\~ao','Interpreter','latex');
-ylabel('[m/s^{2}]','Interpreter','latex');
+ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
+xlabel('Tempo $\rm [s]$','Interpreter','latex');
 xlim(xlimTempo);
 ylim(ylimTempo);
 
 figure(3)
 plot(f,P1);
 title('Espectro de frequ\^encias','Interpreter','latex');
-xlabel('Frequ\^encia [Hz]','Interpreter','latex');
+xlabel('Frequ\^encia $\rm [Hz]$','Interpreter','latex');
+ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
