0a638ab7824552a739b797733594a15e020bd880
Henrique Baron <henrique.baron@gmail.com>
2018-11-06 20:09:11 -0200

    Plotar dados da esfera e do anel interno.

--- ExibirResultados.m
+++ ExibirResultados.m
@@ -6,7 +6,7 @@ ylabel('For\c{c}a $\rm [N]$','Interpreter','latex');
 xlim([0 0.1]);
 ylim('auto');
 
-figure(2)
+fig = figure(2);
 xlimTempo = [0 0.5];
 ylimTempo = 'auto';
 subplot(3,1,1);
@@ -28,8 +28,57 @@ ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
 xlabel('Tempo $\rm [s]$','Interpreter','latex');
 xlim(xlimTempo);
 ylim(ylimTempo);
+fig.WindowState = 'maximized';
 
-figure(3)
+fig = figure(3);
+xlimTempo = [0 0.5];
+ylimTempo = 'auto';
+subplot(3,1,1);
+plot(t,esf.pos);
+title('Esfera','Interpreter','latex');
+ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
+xlim(xlimTempo);
+ylim(ylimTempo);
+
+subplot(3,1,2);
+plot(t,esf.vel);
+ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
+xlim(xlimTempo);
+ylim(ylimTempo);
+
+subplot(3,1,3);
+plot(t,esf.acc);
+ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
+xlabel('Tempo $\rm [s]$','Interpreter','latex');
+xlim(xlimTempo);
+ylim(ylimTempo);
+fig.WindowState = 'maximized';
+
+fig = figure(4);
+xlimTempo = [0 0.5];
+ylimTempo = 'auto';
+subplot(3,1,1);
+plot(t,int.pos);
+title('Anel interno','Interpreter','latex');
+ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
+xlim(xlimTempo);
+ylim(ylimTempo);
+
+subplot(3,1,2);
+plot(t,int.vel);
+ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
+xlim(xlimTempo);
+ylim(ylimTempo);
+
+subplot(3,1,3);
+plot(t,int.acc);
+ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
+xlabel('Tempo $\rm [s]$','Interpreter','latex');
+xlim(xlimTempo);
+ylim(ylimTempo);
+fig.WindowState = 'maximized';
+
+figure(5)
 plot(f,P1);
 title('Espectro de frequ\^encias','Interpreter','latex');
 xlabel('Frequ\^encia $\rm [Hz]$','Interpreter','latex');
