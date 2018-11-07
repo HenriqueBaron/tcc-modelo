b7eb0b258b72c19986384abf9000c930ad96c67e
Henrique Baron <henrique.baron@gmail.com>
2018-11-06 23:07:58 -0200

    Utilizar argumento de entrada em rad/s na função de impulso triangulares.

--- ImpulsosTriangulo.m
+++ ImpulsosTriangulo.m
@@ -1,11 +1,12 @@
 function y = ImpulsosTriangulo(t,f,w)
 %IMPULSOSTRIANGULO Calcula uma funcao de impulsos triangulares.
 %   Calcula uma funcao com impulsos triangulares que ocorrem com uma
-%   frequencia f (em hertz) e tem largura de pulso w.
-kmax = ceil(max(t)*f) + 1;
+%   frequencia angular f e tem largura de pulso w.
+fh = f/(2*pi);
+kmax = ceil(max(t)*fh) + 1;
 yparts = zeros(length(t),kmax+1);
-parfor k=0:kmax
-    yparts(:,k+1) = tripuls(t-k/f,w);
+for k=0:kmax
+    yparts(:,k+1) = tripuls(t-k/fh,w);
 end
 y = sum(yparts,2);
 end
