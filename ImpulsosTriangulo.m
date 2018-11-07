e764cc222ddce7c06c72a9968738f0a418f485bb
Henrique Baron <henrique.baron@gmail.com>
2018-11-06 23:02:51 -0200

    Implementar a função de impulsos triangulares.

--- ImpulsosTriangulo.m
+++ ImpulsosTriangulo.m
@@ -1,6 +1,12 @@
-function y = ImpulsosTriangulo(t,f)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
+function y = ImpulsosTriangulo(t,f,w)
+%IMPULSOSTRIANGULO Calcula uma funcao de impulsos triangulares.
+%   Calcula uma funcao com impulsos triangulares que ocorrem com uma
+%   frequencia f (em hertz) e tem largura de pulso w.
+kmax = ceil(max(t)*f) + 1;
+yparts = zeros(length(t),kmax+1);
+parfor k=0:kmax
+    yparts(:,k+1) = tripuls(t-k/f,w);
+end
+y = sum(yparts,2);
 end
 
