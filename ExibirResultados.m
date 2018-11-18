ea4f4608cc74adc11708439de8f3023d5200b6cf
Henrique Baron <henrique.baron@gmail.com>
2018-11-18 09:51:53 -0200

    Exibir seletivamente os resultados.

--- ExibirResultados.m
+++ ExibirResultados.m
@@ -1,80 +1,96 @@
-figure('Name','Perfil dos impulsos de impacto','NumberTitle','off');
-plot(t,fImpacto(t));
-xlabel('Tempo $\rm [s]$','Interpreter','latex');
-ylabel('For\c{c}a $\rm [N]$','Interpreter','latex');
-xlim([0 0.1]);
-ylim('auto');
+exibirImpactos = true;
+exibirAnelExt = true;
+exibirEsfera = true;
+exibirAnelInt = true;
+exibirEspectroPos = true;
 
-fig = figure('Name','Anel externo','NumberTitle','off');
-xlimTempo = [0 0.5];
-ylimTempo = 'auto';
-subplot(3,1,1);
-plot(t,ext.pos);
-ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
+if exibirImpactos == 1
+    figure('Name','Perfil dos impulsos de impacto','NumberTitle','off');
+    plot(t,fImpacto(t));
+    xlabel('Tempo $\rm [s]$','Interpreter','latex');
+    ylabel('For\c{c}a $\rm [N]$','Interpreter','latex');
+    xlim([0 0.1]);
+    ylim('auto');
+end
 
-subplot(3,1,2);
-plot(t,ext.vel);
-ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
+if exibirAnelExt == 1
+    fig = figure('Name','Anel externo','NumberTitle','off');
+    xlimTempo = [0 0.5];
+    ylimTempo = 'auto';
+    subplot(3,1,1);
+    plot(t,ext.pos);
+    ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
 
-subplot(3,1,3);
-plot(t,ext.acc);
-ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
-xlabel('Tempo $\rm [s]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
-fig.WindowState = 'maximized';
+    subplot(3,1,2);
+    plot(t,ext.vel);
+    ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
 
-fig = figure('Name','Esfera','NumberTitle','off');
-xlimTempo = [0 0.5];
-ylimTempo = 'auto';
-subplot(3,1,1);
-plot(t,esf.pos);
-ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
+    subplot(3,1,3);
+    plot(t,ext.acc);
+    ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
+    xlabel('Tempo $\rm [s]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
+    fig.WindowState = 'maximized';
+end
 
-subplot(3,1,2);
-plot(t,esf.vel);
-ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
+if exibirEsfera == 1
+    fig = figure('Name','Esfera','NumberTitle','off');
+    xlimTempo = [0 0.5];
+    ylimTempo = 'auto';
+    subplot(3,1,1);
+    plot(t,esf.pos);
+    ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
 
-subplot(3,1,3);
-plot(t,esf.acc);
-ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
-xlabel('Tempo $\rm [s]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
-fig.WindowState = 'maximized';
+    subplot(3,1,2);
+    plot(t,esf.vel);
+    ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
 
-fig = figure('Name','Anel interno','NumberTitle','off');
-xlimTempo = [0 0.5];
-ylimTempo = 'auto';
-subplot(3,1,1);
-plot(t,int.pos);
-ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
+    subplot(3,1,3);
+    plot(t,esf.acc);
+    ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
+    xlabel('Tempo $\rm [s]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
+    fig.WindowState = 'maximized';
+end
 
-subplot(3,1,2);
-plot(t,int.vel);
-ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
+if exibirAnelInt == 1
+    fig = figure('Name','Anel interno','NumberTitle','off');
+    xlimTempo = [0 0.5];
+    ylimTempo = 'auto';
+    subplot(3,1,1);
+    plot(t,int.pos);
+    ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
 
-subplot(3,1,3);
-plot(t,int.acc);
-ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
-xlabel('Tempo $\rm [s]$','Interpreter','latex');
-xlim(xlimTempo);
-ylim(ylimTempo);
-fig.WindowState = 'maximized';
+    subplot(3,1,2);
+    plot(t,int.vel);
+    ylabel('Velocidade $\rm [m/s]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
 
-figure('Name','Espectro de frequencias','NumberTitle','off')
-plot(f,P1);
-xlabel('Frequ\^encia $\rm [Hz]$','Interpreter','latex');
-ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
+    subplot(3,1,3);
+    plot(t,int.acc);
+    ylabel('Acelera\c{c}\~ao $\rm [m/s^2]$','Interpreter','latex');
+    xlabel('Tempo $\rm [s]$','Interpreter','latex');
+    xlim(xlimTempo);
+    ylim(ylimTempo);
+    fig.WindowState = 'maximized';
+end
+
+if exibirEspectroPos == 1
+    figure('Name','Espectro de frequencias','NumberTitle','off')
+    plot(f,P1);
+    xlabel('Frequ\^encia $\rm [Hz]$','Interpreter','latex');
+    ylabel('Deslocamento $\rm [m]$','Interpreter','latex');
+end
